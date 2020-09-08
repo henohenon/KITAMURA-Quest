19998c5ae1f6facf34ca79933d0b618ab96a3129
henohenon <takeru_20n1100821@nnn.ed.jp>
2020-09-08 22:16:57 +0900

    js使えね

--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 const maou=document.getElementById("maou");
 const workbutton=document.getElementById("workbutton");
+const buttle_brame=document.getElementById("buttle-frame")
 workbutton.onclick=()=>{
   console.log("ふぁ");
   //maou.setAttribute("animation-mixer","clip: work");
 }
+document.write("ふぁ");
